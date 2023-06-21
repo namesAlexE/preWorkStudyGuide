299d7ccb9831d58b602abfae1e9883e90b4ae043
namesAlexE <alexescalera943@gmail.com>
2023-06-20 17:31:40 -0700

    <added JS code to the console>

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,31 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 
-for (var x = 0; x < topics.length; x++) {
-    console.log(topics[x]);
-}
+function listTopics() {
+    for (var x = 0; x < topics.length; x++) {
+      console.log(topics[x]);
+    }
+  }
+
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+
+  function selectTopic() {
+    if (topics === 'HTML') {
+      console.log("Let's study HTML!");
+    } else if (topics === 'CSS') {
+      console.log("Let's study CSS!");
+    } else if (topics === 'Git') {
+      console.log("Let's study Git!");
+    } else if (topics === 'JavaScript') {
+      console.log("Let's study JavaScript!");
+    } else {
+      console.log('Please try again!');
+    }
+  }
+  console.log('Here are the topics we learned through Prework:');
+  listTopics();
+  console.log('Which topic should we study first?');
+  selectTopic();
 
 //NOTES
 
@@ -18,6 +41,31 @@ for (var x = 0; x < topics.length; x++) {
 // } else {
 //   console.log('Please try again!');
 // }
+
+//FOR LOOPS
+// for (var x = 0; x < topics.length; x++) {
+//     console.log(topics[x]);
+// }
+
+
+//FUCTIONS
+// function helloWorld() {
+//     console.log("Hello, world, I am a function!")
+//    }
+// helloWorld();
+// function selectTopic() {
+//     if (topic === 'HTML') {
+//       console.log("Let's study HTML!");
+//     } else if (topic === 'CSS') {
+//       console.log("Let's study CSS!");
+//     } else if (topic === 'Git') {
+//       console.log("Let's study Git!");
+//     } else if (topic === 'JavaScript') {
+//       console.log("Let's study JavaScript!");
+//     } else {
+//       console.log('Please try again!');
+//     }
+//   }
 
 //EXTRA PRACTICE
 // var shapes = ["triangle", "square", "pentagon", "circle"];
